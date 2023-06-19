a25d4ce1ef6c17605ab7717c502f0d2079723890
ratan kumar jha <imratankj@gamil.com>
2023-06-20 01:03:38 +0530

    error in data_ingestion

---
+++ concrete_strength/constant/training_pipeline/__init__.py
@@ -0,0 +1,4 @@
+# Training pipeline related variable
+TRAINING_PIPELINE_CONFIG_KEY = "training_pipeline_config"
+TRAINING_PIPELINE_ARTIFACT_DIR_KEY = "artifact_dir"
+TRAINING_PIPELINE_NAME_KEY = "pipeline_name"
